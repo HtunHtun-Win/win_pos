eb34e39381ed12d59e99b190d46eb4f47afc36b9
HtunHtunWin <win269127@gmai.com>
2024-08-11 08:58:23 +0630

    finish supplier crud operation

---
+++ lib/contact/supplier/repository/supplier_repository.dart
@@ -0,0 +1,57 @@
+import 'package:jue_pos/core/database/db_helper.dart';
+
+class SupplierRepository{
+  DbHelper dbObj = DbHelper();
+  String TABLE_NAME = "suppliers";
+
+  Future<List> getAll() async{
+    final database = await dbObj.database;
+    return await database.query(TABLE_NAME,where: "isdeleted=0",orderBy: "name");
+  }
+
+  Future<List> getByName(String name) async{
+    final database = await dbObj.database;
+    var data = await database.query(TABLE_NAME,where: "name=? AND isdeleted=0",whereArgs: [name]);
+    return data;
+  }
+
+  Future<int> insert(String name, String phone, String address) async{
+    final database = await dbObj.database;
+    return await database.insert(
+        TABLE_NAME,
+        {
+          "name" : name,
+          "phone" : phone,
+          "address" : address,
+          "created_at" : DateTime.now().toString()
+        }
+    );
+  }
+
+  Future<int> update(int id,String name, String phone, String address) async{
+    final database = await dbObj.database;
+    return await database.update(
+        TABLE_NAME,
+        {
+          "name" : name,
+          "phone" : phone,
+          "address" : address,
+          "created_at" : DateTime.now().toString()
+        },
+        where: "id=?",
+        whereArgs: [id]
+    );
+  }
+
+  Future<void> delete(int id) async{
+    final database = await dbObj.database;
+    await database.update(
+        TABLE_NAME,
+        {
+          "isdeleted" : 1
+        },
+        where: "id=?",
+        whereArgs: [id]
+    );
+  }
+}
---
+++ lib/contact/supplier/screens/supplier_screen.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:jue_pos/contact/customer/screens/customer_add_screen.dart';
+import 'package:jue_pos/contact/supplier/controller/supplier_controller.dart';
+import 'package:jue_pos/contact/supplier/model/supplier_model.dart';
+import 'package:jue_pos/contact/supplier/screens/supplier_add_screen.dart';
+import 'package:jue_pos/contact/supplier/screens/supplier_edit_screen.dart';
+
+
+class SupplierScreen extends StatelessWidget {
+  SupplierScreen({super.key});
+  SupplierController supplierController = Get.put(SupplierController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Obx(()=>ListView.builder(
+        itemCount: supplierController.suppliers.length,
+        itemBuilder: (context,index){
+          var supplier = supplierController.suppliers[index];
+          if(supplier.id==1){
+            return Container();
+          };
+          return listItem(context,supplier);
+        },
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Get.to(()=>SupplierAddScreen());
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget listItem(context,SupplierModel supplier){
+    Color color = Colors.white;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 5,horizontal: 10),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.circular(10)
+      ),
+      child: ListTile(
+        title: Text(
+          supplier.name.toString(),
+          style: TextStyle(
+            color: color,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+        subtitle: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              supplier.phone.toString(),
+              style: TextStyle(
+                  color: color,
+              ),
+            ),
+            Text(
+              supplier.address.toString(),
+              style: TextStyle(
+                  color: color,
+              ),
+            ),
+          ],
+        ),
+        trailing: supplier.id == 1 ? SizedBox() : Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+                onPressed: (){
+                  Get.to(()=>SupplierEditScreen(supplier));
+                },
+                icon: Icon(Icons.edit,color: color,)
+            ),
+            IconButton(
+                onPressed: (){
+                  Get.defaultDialog(
+                      title: "Delete!",
+                      middleText: "Are you sure to delete!",
+                      actions: [
+                        TextButton(onPressed: (){
+                          Get.back();
+                        }, child: Text("Cancel"),),
+                        TextButton(
+                          onPressed: (){
+                            supplierController.delete(supplier.id!);
+                            Get.back();
+                          },
+                          child: Text("Ok"),
+                        ),
+                      ]
+                  );
+                },
+                icon: Icon(Icons.delete,color: color,)
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
