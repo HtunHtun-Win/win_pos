92865888145daab59ff6e2bde88ce0c2b4634430
HtunHtunWin <win269127@gmail.com>
2024-10-10 11:03:08 +0630

    finished inventory-report

---
+++ lib/reports/inventory_reports/screens/inventory_report_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:win_pos/reports/inventory_reports/screens/stock_balance_screen.dart';
+import 'package:win_pos/reports/inventory_reports/screens/stock_balance_valuation_screen.dart';
+
+class InventoryReportScreen extends StatelessWidget {
+  const InventoryReportScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Reports"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: ListView(
+        children: [
+          ListItem(context, const Icon(Icons.inventory), "Stock Balance", () {
+            Get.to(() => StockBalanceScreen());
+          }),
+          ListItem(context, const Icon(Icons.inventory), "Stock Balance With Valuation", () {
+            Get.to(() => StockBalanceValuationScreen());
+          }),
+        ],
+      ),
+    );
+  }
+
+  Widget ListItem(context, icon, text, VoidCallback fun) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 2,horizontal: 8),
+      decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor.withOpacity(1),
+          borderRadius: BorderRadius.circular(10)),
+      child: ListTile(
+        leading: icon,
+        iconColor: Colors.white,
+        title: Text(
+          text,
+          style: const TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+        ),
+        onTap: fun,
+      ),
+    );
+  }
+}
